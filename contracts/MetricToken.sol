82acc543d9c246c43db08df8e4644b1ac35061f4
chance <chance@utc24.io>
2023-01-09 20:45:26 -0600

    feat: layer zero + permits + voting

--- contracts/MetricToken.sol
+++ contracts/MetricToken.sol
@@ -2,12 +2,213 @@
 
 pragma solidity ^0.8.17;
 
+/// @dev Core dependencies.
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MetricToken is ERC20 { 
-    uint256 public maxSupply = 1000000000000000000000000000; // 1 billion tokens
+/// @dev DAO Operations dependencies.
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import {ERC20VotesTimestamp} from "./extensions/ERC20VotesTimestamp.sol";
 
-    constructor() ERC20("Metric Token", "METRIC") {
-        _mint(msg.sender, maxSupply);
+/// @dev Traversal dependencies.
+import {NonBlockingReceiver} from "./extensions/NonBlockingReceiver.sol";
+
+/// @dev Helpers.
+import {ILayerZeroEndpoint} from "./interfaces/ILayerZeroEndpoint.sol";
+
+/**
+ * @title MetricToken
+ * @dev The MetricToken contract is the main token of the Metric protocol. It is used to traverse
+ *     to other chains, and is the main token used to interact with $METRIC protocol service
+ *     offerings and providers.
+ * @author @nftchance*
+ */
+contract MetricToken is
+    ERC20,
+    ERC20Permit,
+    ERC20VotesTimestamp,
+    NonBlockingReceiver
+{
+    /// @dev The max amount of tokens that can be minted.
+    /// @notice 1,000,000,000 tokens minted to the deployer.
+    uint256 public constant maxSupply = 1000000000000000000000000000;
+
+    /// @dev The version of LayerZero being utilized.
+    uint256 public constant layerZeroVersion = 1;
+
+    /// @dev The LayerZeroPayment address.
+    address public constant layerZeroPayment =
+        0x0000000000000000000000000000000000000000;
+
+    /// @dev The amount of gas to send to the destination chain to receive the tokens.
+    uint256 public gasForDestinationLzReceive = 350000;
+
+    ///@dev Encode adapterParams to specify more gas for the destination
+    bytes public adapterParams =
+        abi.encodePacked(layerZeroVersion, gasForDestinationLzReceive);
+
+    ////////////////////////////////////////////////////
+    ///                 CONSTRUCTOR                  ///
+    ////////////////////////////////////////////////////
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        bool _psuedonymBound,
+        address _layerZeroEndpoint
+    )
+        /// @dev Initialize all the dependencies
+        ERC20(_name, _symbol)
+        ERC20Permit(_name)
+        ERC20VotesTimestamp(_psuedonymBound)
+    {
+        /// @dev Mint the total supply of tokens to the deployer.
+        _mint(_msgSender(), maxSupply);
+
+        /// @dev Set the LayerZeroEndpoint address.
+        endpoint = ILayerZeroEndpoint(_layerZeroEndpoint);
+    }
+
+    ////////////////////////////////////////////////////
+    ///                   SETTERS                    ///
+    ////////////////////////////////////////////////////
+
+    /**
+     * @dev Allows any holder of $METRIC to traverse to another chain.
+     * @notice This function is the main entry point for the protocol. It allows users
+     *         to traverse to another chain, and use the protocol on that chain.
+     * @param _chainId The chainId of the chain to traverse to.
+     * @param _amount The amount of tokens to traverse.
+     */
+    function traverse(uint16 _chainId, uint256 _amount) public payable {
+        /// @dev Load the address of the destination chain.
+        bytes storage remoteAddress = orbitLookup[_chainId];
+
+        /// @dev Confirm that the destination chain is currently in orbit.
+        require(
+            remoteAddress.length > 0,
+            "MetricToken: This chain is not currently in orbit."
+        );
+
+        /// @dev Burn the tokens that are traversing to the destination chain to prevent
+        ///      'double minting'.
+        _burn(_msgSender(), _amount);
+
+        ///@dev Encoding the payload with the values to send to the destination chain.
+        bytes memory payload = abi.encode(_msgSender(), _amount);
+
+        /// @dev Get the fees we need to pay to LayerZero + Relayer to cover message delivery
+        /// @notice Surplus funding is refunded to the sender.
+        (uint256 messageFee, ) = endpoint.estimateFees(
+            _chainId,
+            address(this),
+            payload,
+            false,
+            adapterParams
+        );
+
+        /// @dev Confirm that the sender has sent enough funds to cover the traversal fee.
+        require(
+            msg.value >= messageFee,
+            "MetricToken: msg.value not enough to cover traversal fee."
+        );
+
+        /// @dev Send the message to the destination chain.
+        _lzSend(
+            _chainId,
+            payload,
+            payable(msg.sender),
+            layerZeroPayment,
+            adapterParams
+        );
+    }
+
+    /**
+     * @dev Safety function to set the gas for the destination chain to receive the tokens.
+     * @notice This may only be needed in the future if a chain has a higher gas limit than
+     *         the current default of 350,000.
+     * @param _gasForDestinationLzReceive The amount of gas to send to the destination chain
+     */
+    function setGasForDestinationLzReceive(uint256 _gasForDestinationLzReceive)
+        external
+        onlyOwner
+    {
+        gasForDestinationLzReceive = _gasForDestinationLzReceive;
+    }
+
+    /**
+     * @dev Allows MetricsDAO to being operating on new chains.
+     * @notice This functionality allows the DAO and protocol to grow with the ecosystem, and go where
+     *         not only the users are, but what the chain is designed for. The needs of today, will not
+     *         be the needs of tomorrow.
+     * @param _chainId The chainId of the chain to add to the protocol.
+     * @param _orbit The address of the chain to add to the protocol.
+     */
+    function setTrustedRemote(uint16 _chainId, bytes calldata _orbit)
+        external
+        onlyOwner
+    {
+        orbitLookup[_chainId] = _orbit;
+    }
+
+    ////////////////////////////////////////////////////
+    ///               INTERNAL SETTERS               ///
+    ////////////////////////////////////////////////////
+
+    /**
+     * See {NonBlockingReceiver-_LzReceive}.
+     */
+    function _LzReceive(
+        uint16,
+        bytes memory,
+        uint64,
+        bytes memory _payload
+    ) internal override {
+        /// @dev Decode the payload to get the address and amount of tokens traversing.
+        (address to, uint256 amount) = abi.decode(_payload, (address, uint256));
+
+        /// @dev mint the tokens back into existence on destination chain.
+        _mint(to, amount);
+    }
+
+    /**
+     * See {ERC20VotesTimestamp-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20VotesTimestamp) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * See {ERC20VotesTimestamp-_afterTokenTransfer}.
+     */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20VotesTimestamp) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * See {ERC20VotesTimestamp-_mint}.
+     */
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20VotesTimestamp)
+    {
+        super._mint(to, amount);
+    }
+
+    /**
+     * See {ERC20VotesTimestamp-_burn}.
+     */
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20VotesTimestamp)
+    {
+        super._burn(account, amount);
     }
 }
---
+++ contracts/extensions/NonBlockingReceiver.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.17;
+
+/// @dev Core dependencies.
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ILayerZeroReceiver} from "./interfaces/ILayerZeroReceiver.sol";
+
+/// @dev Helpers.
+import {ILayerZeroEndpoint} from "./interfaces/ILayerZeroEndpoint.sol";
+
+abstract contract NonBlockingReceiver is Ownable, ILayerZeroReceiver {
+    ////////////////////////////////////////////////////
+    ///                   STRUCT                     ///
+    ////////////////////////////////////////////////////
+    
+    struct FailedMessages {
+        uint256 payloadLength;
+        bytes32 payloadHash;
+    }
+
+    ////////////////////////////////////////////////////
+    ///                    STATE                     ///
+    ////////////////////////////////////////////////////
+
+    ILayerZeroEndpoint internal endpoint;
+
+    mapping(uint16 => bytes) public orbitLookup;
+
+    mapping(uint16 => mapping(bytes => mapping(uint256 => FailedMessages)))
+        public failedMessages;
+
+    ////////////////////////////////////////////////////
+    ///                    EVENTS                    ///
+    ////////////////////////////////////////////////////
+
+    event MessageFailed(
+        uint16 _srcChainId,
+        bytes _srcAddress,
+        uint64 _nonce,
+        bytes _payload
+    );
+
+    ////////////////////////////////////////////////////
+    ///                   SETTERS                    ///
+    ////////////////////////////////////////////////////
+
+    /**
+     * See {ILayerZeroReceiver.lzReceive}
+     */
+    function lzReceive(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint64 _nonce,
+        bytes memory _payload
+    ) external override {
+        /// @dev Only the LayerZeroEndpoint can call this function.
+        require(msg.sender == address(endpoint));
+        
+        /// @dev Check that the source address is the correct address.
+        require(
+            _srcAddress.length == orbitLookup[_srcChainId].length &&
+                keccak256(_srcAddress) ==
+                keccak256(orbitLookup[_srcChainId]),
+            "NonblockingReceiver: invalid source sending contract"
+        );
+
+        // try-catch all errors/exceptions
+        // having failed messages does not block messages passing
+        try this.onLzReceive(_srcChainId, _srcAddress, _nonce, _payload) {
+            /// @dev There is no need to do anything here.
+        } catch {
+            /// @dev An error or exception occurred, so store the failed message.
+            failedMessages[_srcChainId][_srcAddress][_nonce] = FailedMessages(
+                _payload.length,
+                keccak256(_payload)
+            );
+
+            /// @dev Emit an event to notify the user that a message failed.
+            emit MessageFailed(_srcChainId, _srcAddress, _nonce, _payload);
+        }
+    }
+
+    /**
+     * @dev Confirm the sender of the message before processing the message.
+     * @param _srcChainId The chain ID of the source chain.
+     * @param _srcAddress The address of the source contract.
+     * @param _nonce The nonce of the message.
+     * @param _payload The payload containing the data to be processed.
+     */
+    function onLzReceive(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint64 _nonce,
+        bytes memory _payload
+    ) public {
+        /// @dev Only allow internal transactions.
+        require(
+            msg.sender == address(this),
+            "NonblockingReceiver: caller must be Bridge."
+        );
+
+        /// @dev Handle incoming message.
+        _LzReceive(_srcChainId, _srcAddress, _nonce, _payload);
+    }
+
+    /**
+     * @dev Allows the LayerZeroEndpoint to receive the tokens from the destination chain.
+     * @notice This function is called by the LayerZeroEndpoint contract, and is only callable by
+     *        the LayerZeroEndpoint contract.
+     * @param _srcChainId The chain ID of the source chain.
+     * @param _srcAddress The address of the source contract.
+     * @param _nonce The nonce of the message.
+     * @param _payload The payload containing the address and amount of tokens to receive.
+     */
+    function _LzReceive(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint64 _nonce,
+        bytes memory _payload
+    ) internal virtual;
+
+    /**
+     * @notice Send a message to LayerZero.
+     * @param _dstChainId The chain ID of the destination chain.
+     * @param _payload The payload of the message.
+     * @param _refundAddress The address to refund any remaining value to.
+     * @param _zroPaymentAddress The address to send ZRO payment to.
+     * @param _txParam The transaction parameter of the message.
+     */
+    function _lzSend(
+        uint16 _dstChainId,
+        bytes memory _payload,
+        address payable _refundAddress,
+        address _zroPaymentAddress,
+        bytes memory _txParam
+    ) internal {
+        /// @dev Send the message to the LayerZero endpoint contract.
+        endpoint.send{value: msg.value}(
+            _dstChainId,
+            orbitLookup[_dstChainId],
+            _payload,
+            _refundAddress,
+            _zroPaymentAddress,
+            _txParam
+        );
+    }
+
+    /**
+     * @notice Retry a failed LayerZero message.
+     * @param _srcChainId The chain ID of the source chain.
+     * @param _srcAddress The address of the source contract.
+     * @param _nonce The nonce of the message.
+     * @param _payload The payload of the message.
+     */
+    function retryMessage(
+        uint16 _srcChainId,
+        bytes memory _srcAddress,
+        uint64 _nonce,
+        bytes calldata _payload
+    ) external payable {
+        /// @dev Get the failed message from storage (there may not be one).
+        FailedMessages storage failedMsg = failedMessages[_srcChainId][
+            _srcAddress
+        ][_nonce];
+
+        /// @dev Confirm that there is a failed message that needs retried.
+        require(
+            failedMsg.payloadHash != bytes32(0),
+            "NonblockingReceiver: no stored message"
+        );
+
+        /// @dev Confirm that the payload matches the stored message.
+        require(
+            _payload.length == failedMsg.payloadLength &&
+                keccak256(_payload) == failedMsg.payloadHash,
+            "LayerZero: invalid payload"
+        );
+
+        /// @dev Clear the failed message from storage.
+        failedMsg.payloadLength = 0;
+        failedMsg.payloadHash = bytes32(0);
+
+        /// @dev Try the message again, and revert if it fails again.
+        this.onLzReceive(_srcChainId, _srcAddress, _nonce, _payload);
+    }
+}
